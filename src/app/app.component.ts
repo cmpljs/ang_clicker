e137862a55c73a444e392b312cfdfd40b078fafb
cmpljs <cmplcmplcmpl@gmail.com>
2020-12-15 13:02:30 +0200

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,46 @@
 import { Component } from '@angular/core';
 import { AuthServiceService } from './auth-service.service';
 import { ModeService } from './mode.service';
+import { ClicksServiceService } from './clicks--service.service';
 
 @Component({
   selector: 'app-root',
-  providers: [AuthServiceService, ModeService],
+  providers: [AuthServiceService, ModeService, ClicksServiceService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private _authService: AuthServiceService) {
+  constructor(private _authService: AuthServiceService,
+              private _clicksService: ClicksServiceService) {
+
     _authService.changeEmitted$.subscribe(
       name => {
         this.currentPlayerName = name;
-        this.players.push({name, record: 0});
+        this.records.push([name, 0]);
+      });
+
+      _clicksService.changeEmitted$.subscribe(
+      (clicks:number) => {
+        this.records[this.findCurrentPlayer()][1] = clicks;
+      });
+
+    
+  }
+
+  findCurrentPlayer(): any {
+    this.records.forEach((item, index) => {
+      console.log('item 0: ' + item[0], 'name:' + this.currentPlayerName);
+
+      if (item[0] === this.currentPlayerName) {
+        return index;
       }
-    )
+
+      return -1;
+    })
   }
 
   title = 'ClickerApp';
-  players:Array<{}> = [];
+  records:[string, number][] = [];
   currentPlayerName:string = '';
   currentGameMode:string = '5;'
 
--- src/app/game-play/game-play.component.ts
+++ src/app/game-play/game-play.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModeService } from '../mode.service';
+import { ClicksServiceService } from '../clicks--service.service';
 
 @Component({
   selector: 'app-game-play',
@@ -8,18 +9,40 @@ import { ModeService } from '../mode.service';
 })
 export class GamePlayComponent implements OnInit {
 
-  constructor(private _modeService: ModeService) {
-      _modeService.changeEmitted$.subscribe(
-      mode => {
-        this.secondsLeft = +mode;
-        console.log('GAMEPLAY COMPONENT KNOWS')
-      });
-   }
+  constructor(private _modeService: ModeService,
+              private _clicksService: ClicksServiceService) {}
 
   ngOnInit(): void {
     this.secondsLeft =  +this._modeService.getMode();
   }
 
   secondsLeft:number = 5;
+  btnMsg:string = 'Click to start';
+  gameInProgress:boolean = false;
+  clicks:number = 0;
+
+  finishGame() {
+    this.btnMsg = `You have clicked ${this.clicks} ${this.clicks > 1 ? 'times' : 'time'}!`;
+    this.clicks = 0;
+    this.gameInProgress = false;
+  }
+
+  playGame() {
+    this.gameInProgress = true;
+
+    let timer = setInterval(() => {
+      this.secondsLeft--;
+
+      if (this.secondsLeft <= 0) {
+        clearInterval(timer);
+        this._clicksService.emitChange(this.clicks);
+      }
+    }, 1000)
+  }
+
+  countClicks() {
+    this.clicks++;
+
+  }
 
 }
